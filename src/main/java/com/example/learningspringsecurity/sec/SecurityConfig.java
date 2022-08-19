4554730e4248377e32b9c3c529b9a9914e2e86db
antara-walid <Antara.walid.27@gmail.com>
2022-08-19 10:25:46 +0100

    configuration stateful

--- src/main/java/com/example/learningspringsecurity/sec/SecurityConfig.java
+++ src/main/java/com/example/learningspringsecurity/sec/SecurityConfig.java
@@ -1,33 +1,42 @@
 package com.example.learningspringsecurity.sec;
 
+import com.example.learningspringsecurity.sec.entity.AppUser;
+import com.example.learningspringsecurity.sec.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AccountService accountService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // specify the users that can access resources
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return null;
-            }
-        })
+        auth.userDetailsService(username -> {
+            AppUser appUser = accountService.loadUserByUserName(username);
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            appUser.getAppRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
+            return new User(appUser.getUserName(),appUser.getPassword(),authorities);
+        });
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // specify access rights
-        http.csrf().disable(); // csrf :  cross site request forgery
+//        http.csrf().disable(); // csrf :  cross site request forgery
         http.headers().frameOptions().disable();
         http.formLogin();
         http.authorizeRequests().anyRequest().authenticated();
